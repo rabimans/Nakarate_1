e8f4ed8d6bfaf97c93ef39fcbcb60f7fd8f92007
Yalamber Subba <yalamber@mundhum.com>
2020-06-16 00:34:17 +0545

    udpated config for test

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ import secrets from './secrets';
 export default {
     caption: `
         <a href="https://docs.nakarte.me">Documentation</a> |
-        <a href="https://about.nakarte.me">News</a> |
-        <a href="mailto:nakarte@nakarte.me" target="_self">nakarte@nakarte.me</a> |
+        <a href="https://about.track.guide">News</a> |
+        <a href="mailto:info@track.guide" target="_self">info@track.guide</a> |
         <a href="https://about.nakarte.me/p/blog-page_29.html">Donate</a>`,
-    defaultLocation: [55.75185, 37.61856],
+    defaultLocation: [-33.865143, 151.209900],
     defaultZoom: 10,
     googleApiUrl: `https://maps.googleapis.com/maps/api/js?v=3&key=${secrets.google}`,
     westraDataBaseUrl: 'https://nakarte.me/westraPasses/',
