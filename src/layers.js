c95c93d919d5117bf4544110fd6013c9fe639eca
Greg Lauer <admin@gtechsquared.com>
2020-06-17 19:49:35 +1000

    Updated Thunderbird APIkey

--- src/layers.js
+++ src/layers.js
@@ -450,7 +450,7 @@ import urlViaCorsProxy from '~/lib/CORSProxy';
                 {
                     title: 'OSM Outdoors',
                     isDefault: false,
-                    layer: L.tileLayer('https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png',
+                    layer: L.tileLayer('https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=cf406f30bfc54296a9d573ced1e9d6cf',
                         {
                             code: 'Oso',
                             isOverlay: false,
