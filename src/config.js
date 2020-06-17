8b68d9021f2f3da28ccfec69b4ae246bc80a42c3
Greg Lauer <admin@gtechsquared.com>
2020-06-17 19:59:33 +1000

    Updated spelling mistakes

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ import secrets from './secrets';
 
 export default {
     caption: `
-        <a href="https://help.track.guide">Documentation</a> |
-        <a href="https://about.track.guide">News</a> |
-        <a href="mailto:info@track.guide" target="_self">info@au.track.guidee</a>`,
+        <a href="https://help.track.guide">Help</a> |
+        <a href="https://about.track.guide">About</a> |
+        <a href="mailto:info@track.guide" target="_self">info@track.guide</a>`,
     defaultLocation: [-33.865143, 151.209900],
     defaultZoom: 10,
     googleApiUrl: `https://maps.googleapis.com/maps/api/js?v=3&key=${secrets.google}`,
